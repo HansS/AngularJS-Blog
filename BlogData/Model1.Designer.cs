65d64240f8a958953f7b2c3fcad0475ecb747728
Bobby Kolev <bobbykolev@abv.bg>
2013-09-02 02:09:27 +0300

    new db

--- BlogData/Model1.Designer.cs
+++ BlogData/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'D:\Bobby\Edu\1TERELIK\JS\js-frameworks\hw\AngularJS-Blog\BlogData\Model1.edmx'. 
+﻿// Default code generation is disabled for model 'D:\Bobby\Edu\1TERELIK\JS\jsFW\javaScriptFrameworks.git\trunk\AngularJS-Blog\BlogData\Model1.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
